c0c726729c2b063ea59011b2a6fac75482e080a4
tuychin <tuychin@gmail.com>
2019-09-01 16:59:51 +0300

    Merged navbar & header modules, edited header.scss & index.html

--- src/js/modules/navbar.js
+++ src/js/modules/navbar.js
@@ -1,10 +1,26 @@
 import { ready } from '../utils';
 
 ready(() => { 
-  const toggleBtn = document.querySelector('.navbar__toggle-btn');
-  const navbar = document.querySelector('.navbar__list');
+  const toggleBtn = document.querySelector('.toggle-btn');
+  const navbar = document.querySelector('.navbar');
+  const links = document.querySelectorAll('.navbar__item');
+  const headerLogo = document.querySelector('.header__logo');
 
   toggleBtn.addEventListener('click', (e) => {
-    navbar.classList.toggle('navbar__list_active');
+    navbar.classList.toggle('navbar_open');
+    toggleBtn.classList.toggle('toggle-btn_open');
   });
+
+  for (let link of links) {
+    link.addEventListener('click', (e) => {
+      navbar.classList.remove('navbar_open');
+      toggleBtn.classList.remove('toggle-btn_open');
+    });
+  }
+
+  headerLogo.addEventListener('click', (e) => {
+    navbar.classList.remove('navbar_open');
+    toggleBtn.classList.remove('toggle-btn_open');
+  });
+  
 });
