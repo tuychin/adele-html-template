916192ccaf0c97fb3950813d819280d2a2e2fb03
tuychin <tuychin@gmail.com>
2019-09-11 14:43:56 +0300

    Fix main.js, popup.js, header.js & numbers.js

--- src/blocks/common-blocks/popup/popup.js
+++ src/blocks/common-blocks/popup/popup.js
@@ -1,33 +1,40 @@
 // set popupBtn class
-const popupBtn = document.querySelector('.buttons__video');
-const popup = document.querySelector('.popup');
-const closeBtn = document.querySelector('.popup__close-btn')
-const overlay = popup.querySelector('.popup__overlay');
 
-const videoOff = () => {
-  const popupContent = popup.querySelector('.popup__content').children;
+import { ready } from '../../../js/utils';
 
-  for (let content of popupContent) {
-    if (content.getAttribute('src')) {
-      let oldAttr = content.getAttribute('src');
-
-      content.setAttribute('src', '');
-      content.setAttribute('src', oldAttr);
+ready(() => {
+  
+  const popupBtn = document.querySelector('.buttons__video');
+  const popup = document.querySelector('.popup');
+  const closeBtn = document.querySelector('.popup__close-btn')
+  const overlay = popup.querySelector('.popup__overlay');
+  
+  const videoOff = () => {
+    const popupContent = popup.querySelector('.popup__content').children;
+  
+    for (let content of popupContent) {
+      if (content.getAttribute('src')) {
+        let oldAttr = content.getAttribute('src');
+  
+        content.setAttribute('src', '');
+        content.setAttribute('src', oldAttr);
+      }
     }
   }
-}
+  
+  popupBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    popup.classList.toggle('popup_open');
+  });
+  
+  overlay.addEventListener('click', (e) => {
+    videoOff();
+    popup.classList.remove('popup_open');
+  });
+  
+  closeBtn.addEventListener('click', (e) => {
+    videoOff();
+    popup.classList.remove('popup_open');
+  });
 
-popupBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  popup.classList.toggle('popup_open');
-});
-
-overlay.addEventListener('click', (e) => {
-  videoOff();
-  popup.classList.remove('popup_open');
-});
-
-closeBtn.addEventListener('click', (e) => {
-  videoOff();
-  popup.classList.remove('popup_open');
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,5 @@
 //JavaScript here
 
-import { ready } from '../../../js/utils';
-
-ready(() => {
-
-  import '../blocks/sections/header/header';
-  import '../blocks/common-blocks/popup/popup';
-  import '../blocks/sections/numbers/numbers';
-
-});
+import '../blocks/sections/header/header';
+import '../blocks/common-blocks/popup/popup';
+import '../blocks/sections/numbers/numbers';
