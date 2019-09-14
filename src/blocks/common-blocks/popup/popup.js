85c9e524f37e66f7e8c26e1fd2539175945b82b4
tuychin <tuychin@gmail.com>
2019-09-14 17:00:17 +0300

    Aded slider-block, results-section. Renamed promo to showcase

--- src/blocks/common-blocks/popup/popup.js
+++ src/blocks/common-blocks/popup/popup.js
@@ -3,30 +3,34 @@
 import { ready, videoOff } from '../../../js/utils';
 
 ready(() => {
-  
-  const popupBtn = document.querySelector('.buttons__video');
-  const popup = document.querySelector('.popup');
-  const closeBtn = document.querySelector('.popup__close-btn')
-  const overlay = popup.querySelector('.popup__overlay');
-  const popupVideo = popup.querySelector('iframe');
-  
-  popupBtn.addEventListener('click', (e) => {
-    videoOff(popupVideo);
-    e.preventDefault();
-    popup.classList.toggle('popup_open');
-    document.body.style.overflow = 'hidden';
-  });
-  
-  overlay.addEventListener('click', (e) => {
-    videoOff(popupVideo);
-    popup.classList.remove('popup_open');
-    document.body.style.overflow = 'visible';
-  });
-  
-  closeBtn.addEventListener('click', (e) => {
-    videoOff(popupVideo);
-    popup.classList.remove('popup_open');
-    document.body.style.overflow = 'visible';
-  });
+
+  function modal() {
+    const popupBtn = document.querySelector('.buttons__video');
+    const popup = document.querySelector('.popup');
+    const closeBtn = document.querySelector('.popup__close-btn')
+    const overlay = popup.querySelector('.popup__overlay');
+    const popupVideo = popup.querySelector('iframe');
+    
+    popupBtn.addEventListener('click', (e) => {
+      videoOff(popupVideo);
+      e.preventDefault();
+      popup.classList.toggle('popup_open');
+      document.body.style.overflow = 'hidden';
+    });
+    
+    overlay.addEventListener('click', (e) => {
+      videoOff(popupVideo);
+      popup.classList.remove('popup_open');
+      document.body.style.overflow = 'visible';
+    });
+    
+    closeBtn.addEventListener('click', (e) => {
+      videoOff(popupVideo);
+      popup.classList.remove('popup_open');
+      document.body.style.overflow = 'visible';
+    });
+  }
+
+  modal();
 
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 //JavaScript here
 
-import '../blocks/sections/header/header';
-import '../blocks/common-blocks/popup/popup';
-import '../blocks/sections/numbers/numbers';
+// common blocks
+import '../blocks/common-blocks/header/header.js';
+import '../blocks/common-blocks/popup/popup.js';
+import '../blocks/common-blocks/slider/slider.js';
+
+// sections
+import '../blocks/sections/numbers/numbers.js';
