875138018093b30246a9cbc189e2d007fe03907e
tuychin <tuychin@gmail.com>
2019-09-22 00:46:23 +0300

    Fix header.js

--- src/blocks/common-blocks/header/header.js
+++ src/blocks/common-blocks/header/header.js
@@ -12,18 +12,6 @@ ready(() => {
       navbar.classList.toggle('header__navbar_open');
       toggleBtn.classList.toggle('header__toggle-btn_open');
     });
-  
-    for (let link of navbarLinks) {
-      link.addEventListener('click', (e) => {
-        navbar.classList.remove('header__navbar_open');
-        toggleBtn.classList.remove('header__toggle-btn_open');
-      });
-    }
-  
-    headerLogo.addEventListener('click', (e) => {
-      navbar.classList.remove('header__navbar_open');
-      toggleBtn.classList.remove('header__toggle-btn_open');
-    });
   }
 
   header();
